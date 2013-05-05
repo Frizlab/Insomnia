5903a7230892831a25ea90b576355a5883422f71
Frizlab <fload@me.com>
2013-05-05 17:17:28 +0200

    Project re-created from scratch for OS X Mountain Lion

--- Insomnia/Insomnia.cpp
+++ Insomnia/Insomnia.cpp
@@ -51,9 +51,9 @@ static int sysctl_lidsleep SYSCTL_HANDLER_ARGS;
 static unsigned int lidvar = 1;
 static unsigned int origstate = lidvar;
 
-SYSCTL_PROC(_kern, OID_AUTO, lidsleep, CTLTYPE_INT|CTLFLAG_RW, &lidvar, 0, &sysctl_lidsleep, "I", "");
+SYSCTL_PROC(_kern, OID_AUTO, lidsleep, (int)(CTLTYPE_INT|CTLFLAG_RW), &lidvar, 0, &sysctl_lidsleep, "I", "");
 
-struct kern_ctl_reg ep_ctl; // Initialize control 
+struct kern_ctl_reg ep_ctl; // Initialize control
 kern_ctl_ref kctlref;
 class Insomnia *myinstance;
 
@@ -66,14 +66,14 @@ bool Insomnia::init(OSDictionary* properties) {
 	
 	IOLog("Insomnia:init\n");
 	
-    if (super::init(properties) == false) {
+	if (super::init(properties) == false) {
 		IOLog("Insomnia::init: super::init failed\n");
 		return false;
-    }
+	}
 	
 	myinstance = this;
 	
-    return true;
+	return true;
 }
 
 
@@ -87,73 +87,73 @@ bool Insomnia::start(IOService* provider) {
 	if (!super::start(provider)) {
 		IOLog("Insomnia::start: super::start failed\n");
 		return false;
-    }
+	}
 	
 	sysctl_register_oid(&sysctl__kern_lidsleep);
 	
 	Insomnia::send_event(kIOPMDisableClamshell);// | kIOPMPreventSleep);
 	
-    return true;
+	return true;
 }
 
 
 /* free function for Insomnia, fixed send_event to match other code */
 void Insomnia::free() {
-    IOPMrootDomain *root = NULL;
-    
-    root = getPMRootDomain();
+	IOPMrootDomain *root = NULL;
 	
-    if (!root) {
-        IOLog("Insomnia: Fatal error could not get RootDomain.\n");
-        return;
-    }
-    
+	root = getPMRootDomain();
+	
+	if (!root) {
+		IOLog("Insomnia: Fatal error could not get RootDomain.\n");
+		return;
+	}
+	
 	/* Reset the system to orginal state */
-    Insomnia::send_event(kIOPMAllowSleep | kIOPMEnableClamshell);
+	Insomnia::send_event(kIOPMAllowSleep | kIOPMEnableClamshell);
 	
 	
-    IOLog("Insomnia: Lid close is now processed again.\n");
+	IOLog("Insomnia: Lid close is now processed again.\n");
 	
-    super::free();
-    return;
+	super::free();
+	return;
 }
 
 // ###########################################################################
 
 IOWorkLoop* Insomnia::getWorkLoop() {
-    if (!_workLoop)
-        _workLoop = IOWorkLoop::workLoop();
+	if (!_workLoop)
+		_workLoop = IOWorkLoop::workLoop();
 	
-    return _workLoop;
+	return _workLoop;
 }
 
 // ###########################################################################
 void Insomnia::stop(IOService* provider) {
-    if (_workLoop) {
-        _workLoop->release();
-        _workLoop = 0;
-    }
+	if (_workLoop) {
+		_workLoop->release();
+		_workLoop = 0;
+	}
 	
 	sysctl_unregister_oid(&sysctl__kern_lidsleep);
 	
-    super::stop(provider);
+	super::stop(provider);
 }
 
 #pragma mark -
 
 /* Send power messages to rootDomain */
 bool Insomnia::send_event(UInt32 msg) {
-    IOPMrootDomain *root = NULL;
+	IOPMrootDomain *root = NULL;
 	IOReturn		ret=kIOReturnSuccess;
 	char			err_str[100];
 	
-    IOLog("Insomina: Sending event\n");
+	IOLog("Insomina: Sending event\n");
 	
 	root = getPMRootDomain();
-    if (!root) {
-        IOLog("Insomnia: Fatal error could not get RootDomain.\n");
-        return false;
-    }
+	if (!root) {
+		IOLog("Insomnia: Fatal error could not get RootDomain.\n");
+		return false;
+	}
 	
 	
 	ret = root->receivePowerNotification(msg);
@@ -162,8 +162,8 @@ bool Insomnia::send_event(UInt32 msg) {
 	
 	if(ret!=kIOReturnSuccess)
 	{
-		sprintf(err_str, "Insomina: Error sending event: %d\n", ret);
-		IOLog(err_str);
+		snprintf(err_str, 100, "Insomina: Error sending event: %d\n", ret);
+		IOLog("%s", err_str);
 	}
 	else
 		IOLog("Insomnia: Message sent to root\n");
@@ -186,23 +186,23 @@ IOReturn Insomnia::message(UInt32 type, IOService * provider, void * argument) {
 			
 			Insomnia::send_event( kIOPMClamshellOpened);
 			
-		/* If lastLidState is true - lid closed */
+			/* If lastLidState is true - lid closed */
 		} else if (lastLidState) {
 			IOLog("Insomnia: kClamshellStateBit not set - lid was closed \n");
 			lastLidState = false;
 			
 			// - send kIOPMDisableClamshell | kIOPMPreventSleep here?
 			if(origstate==1)
-				Insomnia::send_event(kIOPMDisableClamshell | kIOPMPreventSleep); 
+				Insomnia::send_event(kIOPMDisableClamshell | kIOPMPreventSleep);
 		}
 		
 		/*		detection of system sleep probably not needed ...
-			
-			if ( argument && kClamshellSleepBit) {
-				IOLog("Insomnia: kClamshellSleepBit set\n - now awake \n");
-			} else {
-				IOLog("Insomnia: kClamshellSleepBit not set - now sleeping \n");
-			}
+		 
+		if ( argument && kClamshellSleepBit) {
+			IOLog("Insomnia: kClamshellSleepBit set\n - now awake \n");
+		} else {
+			IOLog("Insomnia: kClamshellSleepBit not set - now sleeping \n");
+		}
 		*/
 		
 	}
@@ -214,30 +214,29 @@ IOReturn Insomnia::message(UInt32 type, IOService * provider, void * argument) {
 
 static int sysctl_lidsleep SYSCTL_HANDLER_ARGS {
 	int error;
-
+	
 	error = sysctl_handle_int(oidp, oidp->oid_arg1, oidp->oid_arg2,  req);
-
+	
 	if(lidvar!=1 && lidvar!=0)
 		return 1;
 	
 	if (!error && req->newptr) {//we get value
-        if(lidvar != origstate) {
+		if(lidvar != origstate) {
 			/*
 			if(lidvar)//activate
 				myinstance->send_event(kIOPMDisableClamshell | kIOPMPreventSleep);
-			if(!lidvar)//deactivate
-				myinstance->send_event(kIOPMAllowSleep | kIOPMEnableClamshell);
+				if(!lidvar)//deactivate
+					myinstance->send_event(kIOPMAllowSleep | kIOPMEnableClamshell);
 			*/
 			origstate = lidvar;
-			}
-    } else if (req->newptr) {
-        /* Something was wrong with the write request */
-    } else {
-        /* Read request.  */
-        SYSCTL_OUT(req, &lidvar, sizeof(lidvar));
-    }
+		}
+	} else if (req->newptr) {
+		/* Something was wrong with the write request */
+	} else {
+		/* Read request.  */
+		SYSCTL_OUT(req, &lidvar, sizeof(lidvar));
+	}
 	
 	
-
 	return error;
 }
